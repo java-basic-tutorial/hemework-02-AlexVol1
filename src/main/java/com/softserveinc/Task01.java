a3ce85e57c266961f6184a0ba6816a09708f6019
AlexVol1 <ohed@softserveinc.com>
2022-01-25 17:53:11 +0200

    HW2 Task1

--- src/main/java/com/softserveinc/Task01.java
+++ src/main/java/com/softserveinc/Task01.java
@@ -1,5 +1,7 @@
 package com.softserveinc;
 
+import java.sql.SQLOutput;
+
 /*
     Write a program that calculates the average annual temperature.
     Also, you need to find months with the lowest and highest temperature in a year.
@@ -17,8 +19,31 @@ package com.softserveinc;
 public class Task01 {
     public static void main(String[] args) {
                               //  I  | II | III | IV |  V  |  VI | VII | VIII | IX  |  X  | XI |  XII
+        String[] months = {"I"  , "II" , "III" , "IV" ,  "V"  ,  "VI" , "VII" , "VIII" , "IX"  ,  "X"  , "XI" ,  "XII"};
         double[] temperature = { -1.7, -1.0, 2.6,  9.0, 15.1, 19.4, 21.4, 21.2, 17.1, 11.1, 5.9,  1.4 };
         // TODO: Write your code here
+double avgTemp = 0.0;
+for (int i = 0; i < temperature.length; i++) {
+double temp = temperature[i];
+            avgTemp += temp;
+        }
+        avgTemp = avgTemp / temperature.length;
+        System.out.println("Average annual temperature: " + avgTemp);
+int coldestMonthIdx = 0;
+int hottestMonthIdx = 0;
+        for (int i = 1; i < temperature.length; i++) {
+            if (temperature[i] < temperature[coldestMonthIdx]) {
+                coldestMonthIdx = i;
 
+            }
+                if (temperature[i] > temperature[hottestMonthIdx]) {
+                    hottestMonthIdx =i;
+
+                }
+
+
+            }
+        System.out.println("Month with the lowest temperature in a year: " + months[coldestMonthIdx]);
+        System.out.println("The month with the highest temperatures in a year: " + months[hottestMonthIdx]);
+        }
     }
-}
